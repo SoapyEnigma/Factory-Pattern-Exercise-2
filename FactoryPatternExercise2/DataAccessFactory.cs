d03d18c55a344ec40fdf28d78416512c27ff472d
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-26 11:23:18 -0400

    Factory Pattern Exorcise 2

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDataAccessType(DBTypes type)
+        {
+            switch(type)
+            {
+                case DBTypes.LIST: return new ListDataAccess();
+                case DBTypes.SQL: return new SQLDataAccess();
+                case DBTypes.MONGO: return new MongoDataAccess();
+                default: return new ListDataAccess(); // Should error/log
+            }
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true) // not good practice
+            {
+                Console.WriteLine("Hello, what Data would you like to access?\n"
+                    + "[1] List\n"
+                    + "[2] SQL\n"
+                    + "[3] Mongo\n"
+                    + "[4] Exit\n");
+
+                int input;
+                while (!int.TryParse(Console.ReadLine(), out input)
+                    || input != (int)DBTypes.LIST && input != (int)DBTypes.SQL && input != (int)DBTypes.MONGO && input != 4)
+                {
+                    Console.WriteLine("Please only input 1, 2, 3, or 4.");
+                }
+
+                if (input == 4)
+                    break;
+
+                IDataAccess db = DataAccessFactory.GetDataAccessType((DBTypes)input);
+
+                input = 0;
+                while (input != 3)
+                {
+                    Console.WriteLine("What would you like to do?\n"
+                        + "[1] Load Data\n"
+                        + "[2] Save Data\n"
+                        + "[3] Go Back\n");
+
+                    while (!int.TryParse(Console.ReadLine(), out input)
+                        || input != 1 && input != 2 && input != 3)
+                    {
+                        Console.WriteLine("Please only input 1, 2, or 3.");
+                    }
+
+                    if (input == 1)
+                        db.LoadData();
+                    else if (input == 2)
+                        db.SaveData();
+                }
+
+                Console.Clear();
+            }
         }
     }
 }
